f0a8f6830b3c14a1e39ce2d56e672f86b5daf37e
Ilya Ilyin <ilinimm@ya.ru>
2023-05-30 23:15:23 +0300

    Lab05: users items code complete

--- lab05/users_items/src/main/scala/users_items.scala
+++ lab05/users_items/src/main/scala/users_items.scala
@@ -14,5 +14,35 @@ object users_items {
     val outputDir: String = spark.conf.get("spark.users_items.output_dir","/user/ilya.ilyin/visits/users-items")
     val update: String = spark.conf.get("spark.users_items.update","0")
 
+    // Read from HDFS
+    val visitsView = spark.read.option("header", "true").json(inputDir + "/view")
+    val visitsBuy = spark.read.option("header", "true").json(inputDir + "/buy")
+
+    // Union of Buy and View tables
+    val visits = visitsBuy.union(visitsView)
+
+    // Find max date
+    val maxDatePath = visits.select(col("p_date")).orderBy(col("p_date").desc).head()
+
+    // Visits table modification
+    val visitsModified: DataFrame =  visits
+      .filter(col("uid").isNotNull)
+      .filter(col("item_id").isNotNull)
+      .withColumn("item_id", lower(regexp_replace(col("item_id"), "[ ]|[-]", "_")))
+      .withColumn("item_modified", concat(col("event_type"), lit("_"), col("item_id")))
+
+    // Create pivot table (users x items)
+    val visitsPivot: DataFrame  = visitsModified.groupBy(col("uid"), col("item_modified")).agg(count("*").as("cnt"))
+      .groupBy(col("uid")).pivot(col("item_modified")).agg(sum(col("cnt")))
+
+    // Fill null elements
+    val finalDF = visitsPivot.na.fill(0).na.fill("0");
+
+    // Save data to HDFS
+    finalDF.write
+      .format("parquet")
+      .option("path", outputDir + "/" + maxDatePath)
+      .mode("overwrite")
+      .save()
   }
 }
